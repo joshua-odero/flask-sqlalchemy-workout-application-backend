ef09719af85ed8215f66506938dd25417100eabe
joshua-odero <misikojodero@gmail.com>
2026-04-13 22:40:38 +0300

    Initial commit: define models

--- server/models.py
+++ server/models.py
@@ -2,13 +2,38 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
+
 # Define Models here
 class Exercise(db.Model):
-    pass
+    
+    __tablename__ = "exercises"
 
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String)
+    category = db.Column(db.String)
+    equipment_needed = db.Column(db.Boolean)
+
+
+    
 class Workout(db.model):
-    pass
+    
+    __tablename__ = "workouts"
+
+    id = db.Column(db.Integer,primary_key=True)
+    date = db.Column(db.Date)
+    duration_minutes = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+
 
 class WorkoutExercise(db.Model):
-    pass
+    
+    __tablename__ = "workoutexercises"
+
+    id = db.Column(primary_key=True)
+    workout_id = db.Column(db.ForeignKey('workouts.id'))
+    exercise_id = db.Column(db.ForeignKey('exercises.id'))
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
+
 
