c680829875e66ea42f08f634b980de2cbf13e592
joshua-odero <misikojodero@gmail.com>
2026-04-14 08:21:15 +0300

    Initial commit: initialize db and migrate

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,17 @@
 from app import app
 from models import *
+import datetime
 
 with app.app_context():
 
 	# reset data and add new example data, committing to db
     pass
+    
+    
+
+
+
+    
+    
+
+
